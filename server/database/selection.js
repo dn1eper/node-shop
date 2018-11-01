c59c9080a75e8c65e77658b4cb22a4f0424ff83a
andrey <zadneprovskij2.av@edu.spbstu.ru>
2018-11-01 15:29:55 +0200

    Написаны скрипты выборки данных из базы

---
+++ server/database/selection.js
@@ -0,0 +1,11 @@
+module.exports = {
+	allPosts : 'SELECT item_id id, title, htmlText, likes, price FROM item',
+	tagsOfPost : 'SELECT name FROM item_tag WHERE item_id = :item_id',
+	imagesOfPost : 'SELECT url FROM item_image WHERE item_id = :item_id',
+	getPassHash : 'SELECT pass_hash, admin_id FROM admin WHERE login = :login',
+	addNewUser : 'INSERT INTO admin (login, pass_hash) VALUES (:login, :pass_hash)',
+	addNewOrder : 'INSERT INTO booking (user_id) VALUES (:user_id)',
+	addNewOrderItem : 'INSERT INTO booking_items (booking_id, item_id, count) VALUES (:booking_id, :item_id, :count)',
+	removeOrder: 'DELETE FROM booking_items WHERE booking_id = :booking_id',
+	removeOrderItems: 'DELETE FROM booking WHERE booking_id = :booking_id'
+}
